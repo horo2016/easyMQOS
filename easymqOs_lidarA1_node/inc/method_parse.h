19c3963fc209c478724f64dec5d2a6faa70f1f51
horo2016 <selectld@163.com>
2022-12-20 10:58:40 +0800

    Add files via upload

---
+++ easymqOs_lidarA1_node/inc/method_parse.h
@@ -0,0 +1,14 @@
+#ifndef METHOD_PARSE_H
+#define METHOD_PARSE_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern  void save_waypoint();
+extern void clear_waypoint();
+extern void push_waypoint(double lon ,double lat);
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
